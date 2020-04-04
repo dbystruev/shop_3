d03e77c6f8cbedf5ed8c60016598ea9142d8007c
Denis Bystruev <dbystruev@me.com>
2020-04-04 21:09:29 +0300

    Removed debug banner

---
+++ android/app/src/main/kotlin/com/grigarazh/shop_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.grigarazh.shop_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         localizationsDelegates: [S.delegate,
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
